a5d3edfd56db1a33181382b00ba0db4011aae51c
AbhinandanDaksh <117848859+AbhinandanDaksh@users.noreply.github.com>
2024-08-09 10:39:11 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -83,7 +83,7 @@ function Home() {
             <br/>
             <div className="flex justify-center md:justify-start">
               <a
-                href="https://drive.google.com/file/d/1GoI-uLXdykw0Xwnp4g2AykxQNm89orni/view?usp=sharing"
+                href="https://drive.google.com/file/d/1gvwmikI93uPZveHxrpJCB6LI3483U9z_/view?usp=sharing"
                 download="Abhinandan_Resume.pdf"
                 className="bg-orange-500 hover:bg-orange-700 text-white font-bold px-4 py-2 rounded"
               >
