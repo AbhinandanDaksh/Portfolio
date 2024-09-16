bcb03e0c32345a653144394e5ab344688560e18f
AbhinandanDaksh <117848859+AbhinandanDaksh@users.noreply.github.com>
2024-09-16 21:38:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Experiance from "./components/Experiance";
+import Experience from "./components/Experience";
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
 import { Toaster } from "react-hot-toast";
